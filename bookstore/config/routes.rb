aa950dafa2705888f8810168e5f88cd840abd10a
Robi <robi.maliqi@hotmail.co.uk>
2022-10-26 09:57:53 +0100

    added namespace to routes

--- bookstore/config/routes.rb
+++ bookstore/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
+  namespace :api do
+    namespace :v1 do
+    resources :books
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
