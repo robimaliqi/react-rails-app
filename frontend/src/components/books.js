b235fdb3bfb6a419dae3d0228329ad4daad758d6
Robi <robi.maliqi@hotmail.co.uk>
2022-10-26 12:47:33 +0100

    finished front end

--- frontend/src/components/books.js
+++ frontend/src/components/books.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Books() {
-  return <div></div>;
+function Books(props) {
+  return (
+    <div>
+      <h1>These books are from the API</h1>
+      {props.books.map((book) => {
+        return (
+          <div key={book.id}>
+            <h2>{book.title}</h2>
+            <p>{book.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Books;
